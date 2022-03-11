5b3b8e26eb45ed710e37d23247f261cb7866e68c
Mitchell Sayer <mitchell4408@gmail.com>
2022-03-10 20:47:47 -0800

    Added lexograohical unique sort to document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -14,7 +14,24 @@ def main():
 					words[filtered_word] = 1
 
 		words = dict(sorted(words.items(), key = lambda item: item[1], reverse=True))
-		top_five = list(words.items())[:5]
+		dict_items =  list(words.items())
+		top_five = []
+		i = 0
+
+		while len(top_five) < 5 and i < len(dict_items):
+			cur_item = dict_items[i]
+			i += 1
+
+			if len(top_five) == 0:
+				top_five.append(cur_item)
+				continue
+			last_item = top_five[-1]
+
+			if cur_item[1] == last_item[1]:
+				if cur_item[0] < last_item[0]:
+					top_five[-1] = cur_item
+			else:
+				top_five.append(cur_item)
 
 		for item in top_five:
 			print(f'{item[0]}: {item[1]}')
