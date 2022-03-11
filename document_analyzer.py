e586f42a6827d854739a9398a777bbf67cf2abc9
Mitchell Sayer <mitchell4408@gmail.com>
2022-03-10 21:13:20 -0800

    Removed duplicate value logic

--- document_analyzer.py
+++ document_analyzer.py
@@ -8,32 +8,15 @@ def main():
 		for line in f:
 			for word in line.split():
 				filtered_word = re.sub(alphanumeric_re, '', word)
+
 				if filtered_word in words:
 					words[filtered_word] += 1
 				else:
 					words[filtered_word] = 1
 
-		words = dict(sorted(words.items(), key = lambda item: item[1], reverse=True))
-		dict_items =  list(words.items())
-		top_five = []
-		i = 0
+		words = dict(sorted(words.items(), key = lambda item: (-item[1], item[0])))
 
-		while len(top_five) < 5 and i < len(dict_items):
-			cur_item = dict_items[i]
-			i += 1
-
-			if len(top_five) == 0:
-				top_five.append(cur_item)
-				continue
-			last_item = top_five[-1]
-
-			if cur_item[1] == last_item[1]:
-				if cur_item[0] < last_item[0]:
-					top_five[-1] = cur_item
-			else:
-				top_five.append(cur_item)
-
-		for item in top_five:
+		for item in list(words.items())[:5]:
 			print(f'{item[0]}: {item[1]}')
 
 if __name__ == "__main__":
